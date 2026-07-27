e873af95e898742437d49c16ec9b6e0b062b1596
transcriptiolinguae <mesusplit@gmail.com>
2026-07-27 12:25:02 +0200

    Update language.js

--- language.js
+++ language.js
@@ -1,1 +1,30 @@
+// Current language
+export let currentLanguage = "it";
 
+// Dictionary files
+const dictionaries = {
+    it: "./dictionary/phonitalia.json",
+    en: "./dictionary/phonenglish.json",
+    de: "./dictionary/phongerman.json",
+    fr: "./dictionary/phonfrench.json"
+};
+
+// eSpeak voices
+const voices = {
+    it: "it",
+    en: "en",
+    de: "de",
+    fr: "fr"
+};
+
+export function setLanguage(lang){
+    currentLanguage = lang;
+}
+
+export function getDictionaryFile(){
+    return dictionaries[currentLanguage];
+}
+
+export function getVoice(){
+    return voices[currentLanguage];
+}
