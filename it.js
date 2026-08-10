9c95731c0d77c73dcb6a6e8368df7dc35b57bcba
transcriptiolinguae <mesusplit@gmail.com>
2026-08-10 16:10:21 +0200

    Update it.js

--- it.js
+++ it.js
@@ -339,7 +339,7 @@ if (
       // Check if the IPA contains "tʃ" and if it is followed by "i", "ˈi", "j", or "ˈj"
       if (ipa[ipaIndex] === 'l' && ipa[ipaIndex + 1] === 'ˈ' && ipa[ipaIndex + 2] === 'w') {
           result.push(`${text[i]}(ˈl)${text[i + 1]}${text[i + 2]}(w)`);
-          i += 2; // Skip past "ci"
+          i += 3; // Skip past "ci"
           ipaIndex += 3; // Skip past the "tʃ" in IPA
           continue; // Skip the rest of the processing for this "ci"
        }
